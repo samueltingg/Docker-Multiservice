8dd535c1fafbd0330887780747e05f059824b056
Samuel Ting <samuelting7@gmail.com>
2025-08-11 16:07:06 +0800

    feat: remove line that changes root user password

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -20,7 +20,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     done
 
     echo "Creating root user and database..."
-    mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    # mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
     mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
     mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
