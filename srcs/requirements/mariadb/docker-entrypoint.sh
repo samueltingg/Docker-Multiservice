e3be0cdbca3440dfc57435977217efc2d9f24e45
Samuel Ting <samuelting7@gmail.com>
2025-10-08 19:03:58 +0800

    fix: resolve issue where in mariadb container, `mariadb -h"$WORDPRESS_DB_HOST" -u"$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD"` command results to "access denied"

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -14,7 +14,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB database..."
     mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
-    echo "Starting temporary MariaDB..."
+    echo "Starting temporary MariaDB server (for inital setup task)..."
     mysqld_safe --skip-networking &
     pid="$!"
 
@@ -23,11 +23,14 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         sleep 1
     done
 
+    # remove the anonymous users from the mysql.user table:
+    mariadb -e "DELETE FROM mysql.user WHERE User='';"
+
     echo "Creating root user and database..."
     # mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
     mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
     mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
+    mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mariadb -e "FLUSH PRIVILEGES;"
 
     echo "Shutting down temporary MariaDB..."
