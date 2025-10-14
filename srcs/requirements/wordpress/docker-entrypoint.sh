5d6ffbb68426bf3310d4310b3c821569d74dff77
Samuel Ting <samuelting7@gmail.com>
2025-10-14 20:44:15 +0800

    feat: automate wordpress installation in wordpress entrypoint .sh

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -21,11 +21,34 @@ if [ ! -f wp-config.php ]; then
     # fresh installation of wordpress doesn't come with 'wp-config.php',
     #   it only as 'wp-config-sample.php'
     cp wp-config-sample.php wp-config.php
+    # wp.config.php: tells how WordPress should connect to the database
 
+    # update Database Settings in wp-config.php
     sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" wp-config.php
     sed -i "s/username_here/${WORDPRESS_DB_USER}/" wp-config.php
     sed -i "s/password_here/${WORDPRESS_DB_PASSWORD}/" wp-config.php
     sed -i "s/localhost/${WORDPRESS_DB_HOST}/" wp-config.php
+fi
+
+# helper to run wp as www-data (as Wordpress disallow running `wp` command as root)`
+wp_as_www() {
+  runuser -u www-data -- wp "$@"
+}
+
+# After wp-config.php creation and before `exec "$@"`
+if [ ! -f /var/www/html/.installed ]; then
+    echo "Installing WordPress automatically..."
+
+    wp_as_www core install \
+        --url="${WORDPRESS_URL}" \
+        --title="${WORDPRESS_TITLE}" \
+        --admin_user="${WORDPRESS_ADMIN_USER}" \
+        --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+        --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+        --skip-email
+
+    touch /var/www/html/.installed
+    echo "WordPress installation completed!"
 fi
 
 # Fix permissions (systemd would normally ensure service user ownership)
