24581c6a6347eb739ca15f17d29dd781ee653041
Samuel Ting <samuelting7@gmail.com>
2025-10-15 13:23:03 +0800

    fix: in entrypoint, set user permission for 'www-data' before running commands

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -5,6 +5,19 @@ set -e # Exit immediately if any command fails.
 mkdir -p /run/php
 chown -R www-data:www-data /run/php
 
+# Fix permissions (systemd would normally ensure service user ownership)
+chown -R www-data:www-data /var/www/html
+# have to include this command (already present in Dockefile) again,
+#   cuz if I mount a Docker volume(from host) to /var/www/html when starting the container,
+#   the user permissions are replaced by the volume's own user permissions(usually root)
+#   resulting in 'www-data' user losing permission to /var/www/html
+# In short, this command ensure correct permissions for mounted volumes from host
+
+# helper to run wp as www-data (as Wordpress disallow running `wp` command as root)`
+wp_as_www() {
+  runuser -u www-data -- wp "$@"
+}
+
 # Wait for MariaDB before continuing
 if [ -n "$WORDPRESS_DB_HOST" ]; then # check if env var is non-empty
     echo "Waiting for MariaDB at $WORDPRESS_DB_HOST..."
@@ -29,11 +42,6 @@ if [ ! -f wp-config.php ]; then
     sed -i "s/password_here/${WORDPRESS_DB_PASSWORD}/" wp-config.php
     sed -i "s/localhost/${WORDPRESS_DB_HOST}/" wp-config.php
 fi
-
-# helper to run wp as www-data (as Wordpress disallow running `wp` command as root)`
-wp_as_www() {
-  runuser -u www-data -- wp "$@"
-}
 
 # After wp-config.php creation and before `exec "$@"`
 if [ ! -f /var/www/html/.installed ]; then
@@ -60,13 +68,6 @@ if ! wp_as_www user get "$WP_USER_NAME" >/dev/null 2>&1; then
     echo "WordPress user $WP_USER_NAME created with role $WP_USER_ROLE!"
 fi
 
-# Fix permissions (systemd would normally ensure service user ownership)
-chown -R www-data:www-data /var/www/html
-# have to include this command (already present in Dockefile) again,
-#   cuz if I mount a Docker volume(from host) to /var/www/html when starting the container,
-#   the user permissions are replaced by the volume's own user permissions(usually root)
-#   resulting in 'www-data' user losing permission to /var/www/html
-# In short, this command ensure correct permissions for mounted volumes from host
 
 echo "🚀 Starting php-fpm7.4..."
 exec "$@"
