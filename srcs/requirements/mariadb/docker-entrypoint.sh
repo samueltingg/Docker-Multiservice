085a057d26e1d79f8f763b9332f62e68a4737073
Samuel Ting <samuelting7@gmail.com>
2025-10-15 09:51:33 +0800

    chore: added comment

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -26,17 +26,19 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     # remove the anonymous users from the mysql.user table:
     mariadb -e "DELETE FROM mysql.user WHERE User='';"
 
-    echo "Creating root user and database..."
-    # mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    echo "Creating wordpress database and user..."
     mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
     mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mariadb -e "FLUSH PRIVILEGES;"
-
+    
+    # echo "Setting root user password..."
+    # mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    
     echo "Shutting down temporary MariaDB..."
     mariadb-admin shutdown
     wait "$pid"
 fi
 
 echo "🚀 Starting MariaDB..."
-exec gosu mysql "$@"
+exec gosu mysql "$@" # run mysqld_safe as the Linux user `mysql` (running database servers as root is unsafe)
